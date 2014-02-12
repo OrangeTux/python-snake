36178132ddb1242dc9e61751cd18da44847870d0
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-02-13 00:18:52 +0100

    Implemented snake module.

---
+++ snake/snake.py
@@ -0,0 +1,36 @@
+class Snake:
+    """ Model for snake. It can move its head, tail and can grow.
+
+    :param size:    The begin size of the snake.
+
+    """
+    def __init__(self, size=3):
+        self.segments = list()
+        for y in range(size):
+            self.segments.append((0, y))
+
+    def move_head(self, x, y):
+        """ Move head to the snake to location.
+
+        :param x:   The x coordinate of new location.
+        :param y:   The y coordinate of the new location.
+
+        """
+        self.segments.insert(0, (x, y))
+
+    def move_tail(self):
+        """ Remove last segment of snake. """
+        self.segments.pop()
+
+    def move(self, x, y, grow=False):
+        """ Move head and tail of snake.
+
+        :param x:       The x coordinate of new location of the head.
+        :param y:       The y coordinate of the new location of the head.
+        :param grow:    If snake has to grow or not.
+
+        """
+        if not grow:
+            self.move_tail()
+
+        self.move_head(x, y)
--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,9 +1,10 @@
 from snake.snake import Snake
+from copy import copy
 
 
 def test_amount_of_segments():
     """ Test amount of segments of snake. """
-    s = Snake(length=3)
+    s = Snake(size=3)
     assert len(s.segments) is 3
 
 
@@ -11,33 +12,47 @@ def test_move_head(snake):
     """ Test move_head(). """
     x = 0
     y = 3
-    old_segments = snake.segments
+    old_segments = copy(snake.segments)
 
     snake.move_head(x, y)
-    assert snake.segments is len(old_segments) + 1
-    assert snake.segements[0] is (x, y)
+    assert len(snake.segments) is len(old_segments) + 1
+    assert snake.segments[0] == (x, y)
 
 
 def test_move_tail(snake):
     """ Test move_tail(). """
-    old_segments = snake.segments
+    old_segments = copy(snake.segments)
 
     snake.move_tail()
     assert old_segments.pop() not in snake.segments
-    assert old_segments in snake.segments
+    assert old_segments == snake.segments
     assert len(snake.segments) is 2
 
 
 def test_move_with_grow(snake):
     """ Test if snake moves correctly. """
-    old_segments = snake.segments
+    old_segments = copy(snake.segments)
     snake.move(0, 3, grow=True)
 
     # Snake hasn't grew, so body is same size.
-    assert len(snake.segments) + 1 is len(old_segments)
+    assert len(snake.segments) is len(old_segments) + 1
     # Head is now placed on te new location.
-    assert snake.segments[0] is (0, 3)
+    assert snake.segments[0] == (0, 3)
     # Previous head is now second segment.
-    assert old_segments[1] is snake.segments[0]
+    assert old_segments[0] is snake.segments[1]
     # The tail is not on same location.
     assert old_segments.pop() in snake.segments
+
+def test_move_without_grow(snake):
+    """ Test if snake moves correctly. """
+    old_segments = copy(snake.segments)
+    snake.move(0, 3, grow=False)
+
+    # Snake hasn't grew, so body is same size.
+    assert len(snake.segments) is len(old_segments)
+    # Head is now placed on te new location.
+    assert snake.segments[0] == (0, 3)
+    # Previous head is now second segment.
+    assert old_segments[0] is snake.segments[1]
+    # The tail is not on same location.
+    assert old_segments.pop() not in snake.segments
